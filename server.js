24f36422ee06ac82b5a8be1b209d7e665b5bcf03
Ompandey12 <p.ompandey2012@gmail.com>
2024-11-08 14:23:10 +0530

    cors

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ dotenv.config();
 const app = express();
 app.use(express.static('public'));
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 // Endpoint to get environment variables
 app.get('/api/config', (req, res) => {
     res.json({
